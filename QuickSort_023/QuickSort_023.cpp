4088059d6584ca936875b04bd3ced49fec55628a
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-30 10:35:36 +0700

    Add project files.

---
+++ QuickSort_023/QuickSort_023.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+int arr[20];
+int cmp_cout = 0; // number of comparasion
+int mov_cout = 0; // number of data movements
+int n;
