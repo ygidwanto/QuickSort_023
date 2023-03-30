de782af575555b7456e005e732de4659f4969057
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-30 11:35:29 +0700

    melakukan implementasi data pada quick sort

--- QuickSort_023/QuickSort_023.cpp
+++ QuickSort_023/QuickSort_023.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 //array of integers to hold values
 int arr[20];
-int cmp_cout = 0; // number of comparasion
-int mov_cout = 0; // number of data movements
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
 int n;
 
 void input() {
@@ -37,4 +37,61 @@ void swap(int x, int y)
 	temp = arr[x];
 	arr[x] = arr[y];
 	arr[y] = temp;
+}
+
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)
+		return;                                               // Langkah 1
+
+	//Partition the list into two part:
+	//one containing elements less that or equal to pivot
+	//Pither conntaining elements greather than pivot 
+
+	pivot = arr[low];                                         // Langkah 2
+
+	i = low + 1;                                              // Langkah 3
+	j = high;                                                 // Langkah 4
+
+
+	while (i <= j)
+	{
+		//search for an element greater than pivot
+		while ((arr[j] <= pivot) && (i <= high))             // Langkah 5
+		{
+			i++;                                             // Langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low))               // Langkah 7
+		{
+			j--;                                             // Langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
+
+		                                                     // Langkah 9
+		if (i < j) // if the greater element is ont the left of the element
+		{
+			//Swap the element at index i whit the element in at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
+	//j now containt the index of the last element in the sorted list
+	if (low < j);											// Langkah 11
+	{
+		//move the pivot to its correct position in the list 
+		swap(low, j);
+		mov_count++;
+	}
+
+
+	//sort the list list on the left of pivot using quck sort
+	q_short(low, j - 1);                                    // Langkah 12
+	//Short the list on the right of pivot using quick sort
+	q_short(j + 1, high);                                   // Langkah 13
+
 }
