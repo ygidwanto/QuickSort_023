30bca6478319de5d2a9b6f015bc6de99ee80f91c
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-30 11:46:25 +0700

    untuk menampillkan hasill

--- QuickSort_023/QuickSort_023.cpp
+++ QuickSort_023/QuickSort_023.cpp
@@ -95,3 +95,18 @@ void q_short(int low, int high)
 	q_short(j + 1, high);                                   // Langkah 13
 
 }
+
+
+void display() {
+	cout << "\n=======================" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "===================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparations: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
