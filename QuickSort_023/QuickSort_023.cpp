a404974b544e8c15c7714cf0ff2379bb6e626a7d
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-30 10:58:08 +0700

    swaps the element index x with the elemnt index y

--- QuickSort_023/QuickSort_023.cpp
+++ QuickSort_023/QuickSort_023.cpp
@@ -29,3 +29,12 @@ void input() {
 		cin >> arr[i];
 	}
 }
+ // swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
