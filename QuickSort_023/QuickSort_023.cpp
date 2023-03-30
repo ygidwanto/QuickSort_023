7908384a2293781b0a8c3a52870352652453b848
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-30 10:47:16 +0700

    Memasukan elemen array

--- QuickSort_023/QuickSort_023.cpp
+++ QuickSort_023/QuickSort_023.cpp
@@ -6,3 +6,16 @@ int arr[20];
 int cmp_cout = 0; // number of comparasion
 int mov_cout = 0; // number of data movements
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Memasukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+}
